35a689566a37a2470f27988953c28969a6b34751
juliasouz <juliasouza1308@outlook.com>
2025-06-15 22:27:50 -0300

    feat: make Greeting display dynamic name instead of 'World' in jsx best practices exercise

--- 01 - React Fundamentals/src/index.jsx
+++ 01 - React Fundamentals/src/index.jsx
@@ -1,7 +1,26 @@
+import React from "react";
 import ReactDOM from "react-dom/client";
 
 function Greeting() {
-  return <h2>Component</h2>;
+  const [name, setName] = React.useState("");
+
+  return (
+    <div style={{ textAlign: "center", fontFamily: "Arial" }}>
+      <h1>Greetings!</h1>
+      <ul style={{ listStyle: "none", padding: 0 }}>
+        <li>
+          <p>What's your name?</p>
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            style={{ padding: "8px", fontSize: "16px" }}
+          />
+        </li>
+      </ul>
+      <h2>Hello, {name || "World"}!</h2>{" "}
+    </div>
+  );
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
